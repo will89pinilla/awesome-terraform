3a2de5121b5481a6469ec2b95d8b4c25023ce2d3
William Pinilla <andrespinillazamudio@gmail.com>
2020-06-11 10:52:48 -0500

    trying from zero using terraform

--- Terraform/swap/swap.tf
+++ Terraform/swap/swap.tf
@@ -1,14 +1,22 @@
-# Configure the Azure provider
-provider "azurerm" { 
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you are using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
+terraform {
+     required_version = ">= 0.11" 
+     backend "azurerm" {
+         storage_account_name   = "__terraformstorageaccount__"
+        container_name          = "terraform"
+        key                     = "terraform.tfstate"
+	    access_key              = "__storagekey__"
+        features{}
+    }
+}
+
+provider "azurerm" {
+    version = "=2.0.0"
     features {}
 }
 
 # Swap the production slot and the staging slot
 resource "azurerm_app_service_active_slot" "slotDemoActiveSlot" {
     resource_group_name   = "slotDemoResourceGroup"
-    app_service_name      = "slotAppServiceWill"
-    app_service_slot_name = "lotAppServiceSlotOneWill"
+    app_service_name      = "__appservicename__"
+    app_service_slot_name = "__appservicenameslot__"
 }
