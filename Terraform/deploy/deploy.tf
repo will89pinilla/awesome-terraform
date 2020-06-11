97f066426ba7ce22c50f4a858ad446c4fc31d359
William Pinilla <andrespinillazamudio@gmail.com>
2020-06-11 12:10:50 -0500

    solve some script issues

--- Terraform/deploy/deploy.tf
+++ Terraform/deploy/deploy.tf
@@ -2,10 +2,10 @@ terraform {
      required_version = ">= 0.11" 
      backend "azurerm" {
          storage_account_name   = "__terraformstorageaccount__"
-        container_name          = "terraform"
-        key                     = "terraform.tfstate"
-	    access_key              = "__storagekey__"
-        features{}
+         container_name          = "terraform"
+         key                     = "terraform.tfstate"
+         access_key              = "__storagekey__"
+         features{}
     }
 }
 
