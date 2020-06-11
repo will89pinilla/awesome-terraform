5ad3f850904f586f77e277a191b03a2703965500
William Pinilla <andrespinillazamudio@gmail.com>
2020-06-11 16:28:00 -0500

    add a change on the tf file

--- Terraform/deploy/deploy.tf
+++ Terraform/deploy/deploy.tf
@@ -1,8 +1,17 @@
-# Configure the Azure provider
-provider "azurerm" { 
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you are using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
+terraform {
+     required_version = ">= 0.11" 
+     backend "azurerm" {
+         storage_account_name   = "__terraformstorageaccount__"
+         container_name          = "terraform"
+         key                     = "terraform.tfstate"
+         access_key              = "__storagekey__"
+         features{}
+    }
+}
+
+
+provider "azurerm" {
+    version = "=2.0.0"
     features {}
 }
 
@@ -12,7 +21,7 @@ resource "azurerm_resource_group" "slotDemo" {
 }
 
 resource "azurerm_app_service_plan" "slotDemo" {
-    name                = "slotAppServicePlan"
+    name                = "__appserviceplan__"
     location            = azurerm_resource_group.slotDemo.location
     resource_group_name = azurerm_resource_group.slotDemo.name
     sku {
@@ -22,14 +31,14 @@ resource "azurerm_app_service_plan" "slotDemo" {
 }
 
 resource "azurerm_app_service" "slotDemo" {
-    name                = "slotAppServiceWill"
+    name                = "__appservicename__"
     location            = azurerm_resource_group.slotDemo.location
     resource_group_name = azurerm_resource_group.slotDemo.name
     app_service_plan_id = azurerm_app_service_plan.slotDemo.id
 }
 
 resource "azurerm_app_service_slot" "slotDemo" {
-    name                = "slotAppServiceSlotOneWill"
+    name                = "__appservicenameslot__"
     location            = azurerm_resource_group.slotDemo.location
     resource_group_name = azurerm_resource_group.slotDemo.name
     app_service_plan_id = azurerm_app_service_plan.slotDemo.id
